850054b832ed107233e0a6b07e8a4ea18452d55a
iUnknownWs <109998892+iUnknownWs@users.noreply.github.com>
2024-01-19 12:02:13 -0400

    prepare vercel

--- fundatur/settings.py
+++ fundatur/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1rzgy=ho(6g#z2m4*ez7q^4lm53e%vt51xk$qc&cbefhv9=d!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
